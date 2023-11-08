915018b45018af067e1063567d18e10e54e1fa1b
Brent Ritchie <brent.ritchie@cambriancollege.ca>
2023-11-07 20:56:17 -0500

    more tests.

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -5,8 +5,6 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Edge;
-using OpenQA.Selenium.DevTools.V118.Network;
-using OpenQA.Selenium.Chromium;
 
 namespace CMP_1005_Fall_2023_Testing;
 
@@ -25,31 +23,58 @@ public class UnitTest1
     public void Setup()
     {
         var opts = new ChromeOptions();
-        opts.PageLoadStrategy = PageLoadStrategy.Eager;
+        // opts.PageLoadStrategy = PageLoadStrategy.Eager;
         // opts.EnableMobileEmulation("iPhone XR");
         _webDriver = new ChromeDriver(opts);
     }
 
-    // [TestMethod]
-    // public void TestMethod1()
-    // {
-    //     _webDriver.Navigate().GoToUrl("https://www.google.ca");
-    //     Assert.IsTrue(_webDriver.Title.Contains("Google"));
-    // }
+    [TestMethod]
+    public void TestMethod1()
+    {
+        _webDriver.Navigate().GoToUrl("https://www.webscraper.io/test-sites/e-commerce/static");
+        var heading = _webDriver.FindElement(By.CssSelector(".jumbotron > h1"));
+        // Console.WriteLine(heading.Text);
+        Assert.IsTrue(heading.Text.Contains("training"));
+    }
 
     [TestMethod]
     public void TestMethod2()
     {
-        // Textbox - name=q
-        // input - name=btnK
-        _webDriver.Navigate().GoToUrl("https://www.google.ca");
-        var searchBox = _webDriver.FindElement(By.CssSelector("textarea[name=q]"));
-        var searchBtn = _webDriver.FindElement(By.CssSelector("input.gNO89b"));
+        _webDriver.Navigate().GoToUrl("https://www.webscraper.io/test-sites/e-commerce/static");
+        var phoneLink = _webDriver.FindElement(By.CssSelector("a.category-link[href$=phones]"));
+        phoneLink.Click();
+        var touchLink = _webDriver.FindElement(By.CssSelector("a.subcategory-link[href$=touch]"));
+        Assert.IsNotNull(touchLink);
+        Assert.AreEqual("Touch", touchLink.Text);
+    }
 
-        searchBox.SendKeys("Hello");
-        searchBtn.Click();
+    [TestMethod]
+    public void TestMethodFindScrapedHeading()
+    {
+        _webDriver.Navigate().GoToUrl("https://www.webscraper.io/test-sites/e-commerce/static/phones");
+        var heading = _webDriver.FindElement(By.CssSelector(".col-lg-9 > h2"));
+        Assert.IsTrue(heading.Text.Equals("Top items being scraped right now"));        
+        Assert.IsTrue(heading.Text.Contains("scraped"));
+    }
 
-        Assert.IsTrue(_webDriver.Title.Contains("Hello"));
+    [TestMethod]
+    public void TestMethod3()
+    {
+        _webDriver.Navigate().GoToUrl("https://www.webscraper.io/test-sites/e-commerce/static");
+       var topItems =_webDriver.FindElement(By.CssSelector(".jumbotron + h2"));
+       Assert.IsTrue(topItems.Text.Contains("scraped"));
+    }
+
+    [TestMethod]
+    public void TestMethod4()
+    {
+        _webDriver.Navigate().GoToUrl("https://www.webscraper.io/test-sites/e-commerce/static");
+    //    var topItems =_webDriver.FindElement(By.CssSelector(".jumbotron + h2"));
+    //    Assert.IsTrue(topItems.Text.Contains("scraped"));
+       var Lenovo = _webDriver.FindElement(By.LinkText("Lenovo ThinkPad Yoga 370 Black"));
+       Lenovo.Click();
+       var Price = _webDriver.FindElement(By.ClassName("float-end price pull-right"));
+       Assert.AreEqual("$1362.24", Price.Text);
     }
 
     [TestCleanup]
